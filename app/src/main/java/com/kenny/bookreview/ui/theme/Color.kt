93236779d7c7b673b9a04ab9f56b45a1fe0bc1d8
chang <kennethchangla@gmail.com>
2022-06-20 16:41:05 +0800

    [ chore ] set up project color and theme

--- app/src/main/java/com/kenny/bookreview/ui/theme/Color.kt
+++ app/src/main/java/com/kenny/bookreview/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.kenny.bookreview.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Indigo200 = Color(0xFF9FA8DA)
+val Indigo500 = Color(0xFF3F51B5)
+val Indigo700 = Color(0xFF303F9F)
+val BlueGray200 = Color(0xFFB0BEC5)
+val BlueGray500 = Color(0xFF607D8B)
+val BlueGray700 = Color(0xFF455A64)
